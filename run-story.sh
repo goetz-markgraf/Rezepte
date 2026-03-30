0b6b50562376c492cd835d10bd51031b96510bfd
Goetz Markgraf <goetz.markgraf@freenet.de>
2026-03-30 12:14:51 +0200

    update run script for 1 story

--- run-story.sh
+++ run-story.sh
@@ -1,9 +1,9 @@
-echo ###############################################
+echo ===============================================
 echo ### implementing the next story
-echo ###############################################
+echo ===============================================
 opencode run "@.opencode/commands/run-story.md" --model "portkey evaluation/@openrouter-eval/moonshotai/kimi-k2.5"
 
-echo ###############################################
+echo ===============================================
 echo ### sending completion message to user
-echo ###############################################
+echo ===============================================
 osascript ~/bin/send_question.applescript "Story abgeschlossen"
