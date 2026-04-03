3d8373bf32211e63397d5aa228bfd96c633b6bfd
Goetz Markgraf <goetz.markgraf@freenet.de>
2026-04-03 14:00:57 +0200

    WIP try release for NAS

--- compile_for_nas.sh
+++ compile_for_nas.sh
@@ -2,12 +2,15 @@
 set -e
 
 TARGET=armv7-unknown-linux-gnueabihf
+# GLIBC 2.26 targeten (Version auf dem NAS)
+ZIG_TARGET="${TARGET}.2.26"
 
 echo "Rust-Target sicherstellen..."
 rustup target add "$TARGET"
 
-echo "Baue Binary für NAS (ARMv7)..."
-cargo zigbuild --release --target "$TARGET"
+echo "Baue Binary für NAS (ARMv7, GLIBC 2.26, Cortex-A5)..."
+# RUSTFLAGS: -C target-cpu=cortex-a5 für ältere ARMv7 ohne NEON
+RUSTFLAGS="-C target-cpu=cortex-a5" cargo zigbuild --release --target "$ZIG_TARGET"
 
 echo ""
 echo "Fertig: target/$TARGET/release/rezepte"
